22c33211c6725eff51ddda99e60bf1a8185c3623
Евгений Чайка <chay67ka26@gmail.com>
2023-12-22 17:30:53 +0300

    add update and delete routes for users/, create views

--- task_manager/users/urls.py
+++ task_manager/users/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
-from task_manager.users.views import IndexView
+from task_manager.users.views import (IndexView, UserFormUpdateView,
+                                      UserFormDeleteView)
 
 urlpatterns = [
     path('', IndexView.as_view(), name='users'),
+    path('<int:id>/update/', UserFormUpdateView.as_view(), name='user_update'),
+    path('<int:id>/delete/', UserFormDeleteView.as_view(), name='user_delete'),
 ]
--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, HttpResponse
 from django.views import View
 
 
@@ -15,3 +15,35 @@ class IndexView(View):
         return render(request, 'users/index.html', context={
             'users': users,
         })
+
+
+class UserFormUpdateView(View):
+
+    def get(self, request, *args, **kwargs):
+        return HttpResponse('GET request to users/{id}/update')
+        # form = UserForm()
+        # return render(request, 'users/create.html', context={
+        #     'form': form,
+        # })
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('POST request to users/{id}/update')
+        # form = UserForm(request.POST)
+        # if form.is_valid():
+        #     form.save()
+        #     return redirect('users')
+        # messages.warning(request, 'Incorrect data!')
+        # return render(request, 'users/create.html', context={
+        #     'form': form})
+
+
+class UserFormDeleteView(View):
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('POST request to users/{id}/delete')
+        # user_id = kwargs.get('id')
+        # user = User.objects.get(id=user_id)
+        # if user:
+        #     user.delete()
+        #     messages.warning(request, 'Пользователь удален')
+        # return redirect('users')
