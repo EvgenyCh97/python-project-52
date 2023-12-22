19000730a3d7b3cd0ec1c20a1a3591e340e4d36e
Евгений Чайка <chay67ka26@gmail.com>
2023-12-22 17:29:52 +0300

    update IndexView, create users context var for users/index.html template

--- task_manager/users/views.py
+++ task_manager/users/views.py
@@ -5,7 +5,13 @@ from django.views import View
 class IndexView(View):
 
     def get(self, request, *args, **kwargs):
-        users = ['Ivan', 'Alex', 'Sansa']
+        users = [{'id': 1, 'name': 'Alex', 'full_name': 'Alex Petrov',
+                  'created_at': '2023-12-22'},
+                 {'id': 2, 'name': 'Sansa', 'full_name': 'Sansa Stark',
+                  'created_at': '2023-12-20'},
+                 {'id': 3, 'name': 'Lev', 'full_name': 'Lev Tolstoy',
+                  'created_at': '1950-10-24'}
+                 ]
         return render(request, 'users/index.html', context={
             'users': users,
         })
